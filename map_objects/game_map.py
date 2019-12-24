f260d3831556f10988a7248755c4fdbf76897cd1
Borisko130 <krimenyukb@gmail.com>
2019-12-24 23:01:01 +0200

    Added Rect class and create room func in GameMap

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -7,17 +7,17 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-
-        tiles[30][22].blocked = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
     
+    def create_room(self, room):
+        # Go through the tiles in the rectangle and make them passable
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+
 
     def is_blocked(self, x, y,):
         if self.tiles[x][y].blocked:
