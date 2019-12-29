6db1a31908bbe6263e82d24256e2ffd35a344f37
Borisko130 <krimenyukb@gmail.com>
2019-12-29 22:03:51 +0200

    Added function to place entities(currently enemies) in the dungeon

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,6 +1,8 @@
+from tcod as libtcod
 from random import randint
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
+from entity import Entity
 
 """
 Generates map
@@ -94,6 +96,24 @@ class GameMap:
         for y in range(min(y1, y2), max (y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
+    
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        # Get a random number of monters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green)
+                else:
+                    monster = Entity(x, y, 'T', libtcod.darker_green)
+
+                entities.append(monster)
 
 
     def is_blocked(self, x, y,):
